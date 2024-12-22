f157a972e9337844c9e6c97ad4e8e6b37f8d35c6
B Lemarr <blake.lemarr@outlook.com>
2024-12-21 20:20:38 -0500

    will need notations

---
+++ imports-exports/commonjs-import.js
@@ -0,0 +1,10 @@
+// CommonJS import uses the require() function
+
+const cjs = require('./commonjs-export');
+
+let msg = cjs.msg;
+let logMsg = cjs.logMsg;
+let msg2 = cjs.msg2;
+let logMsg2 = cjs.logMsg2;
+let message = cjs.message;
+let logMessage = cjs.logMessage;
