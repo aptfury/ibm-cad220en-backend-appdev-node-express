90e6a7bd4d006c7e00eb0a310dbb3956f2fe93a7
aptfury <blake.lemarr@outlook.com>
2024-12-28 06:49:48 -0500

    Added promises lab tasks

---
+++ classes/node-express/labs/m02-mjs-promises-callbacks/promisescript-task02.mjs
@@ -0,0 +1,20 @@
+// Task Two
+let firstPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("6 second promise resolved");
+    }, 6000);
+});
+
+let secondPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("3 second promise resolved");
+    }, 3000);
+});
+
+firstPromise.then((successMessage) => {
+    console.log(`From Callback: ${successMessage}`);
+});
+
+secondPromise.then((successMessage) => {
+    console.log(`From Callback: ${successMessage}`);
+});
